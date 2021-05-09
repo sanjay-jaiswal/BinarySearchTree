b7764a7502d8f794ad7a49db5da7b76c33acb6df
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-09 19:36:28 +0530

    [Sanju] ADD : Welcome meaasage.

---
+++ Day17_BinarySearchTree/Day17_BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day17_BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================ Welcome To Binary Search Tree (BST) Program ==========================");
+        }
+    }
+}
